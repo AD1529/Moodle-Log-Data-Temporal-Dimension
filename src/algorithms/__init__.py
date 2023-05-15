340190ac78136a2869bd72904a4e04bc319f4581
Daniela Rotelli <65086299+AD1529@users.noreply.github.com>
2023-05-15 15:15:43 +0200

    Update __init__.py

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,9 +1,9 @@
-__all__ = ["Records", "categorisation", "filtering", "duration", "extracting", "integrating", "sorting"]
+__all__ = ["Records", "categorisation", "duration", "extracting", "filtering", "integrating", "sorting"]
 
 from src.classes.records import Records
 from .categorisation import *
-from .filtering import *
 from .duration import *
 from .extracting import *
+from .filtering import *
 from .integrating import *
 from .sorting import *
