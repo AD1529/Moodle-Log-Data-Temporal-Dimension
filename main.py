00feb4da45535bfb3a49f583f595d3eaf3d6f51e
Daniela Rotelli <65086299+AD1529@users.noreply.github.com>
2023-05-16 12:18:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
 
     FILE_PATH = 'src/datasets/consolidated_df.csv'
 
-    logs = get_categorical_duration(FILE_PATH)
+    records = get_categorical_duration(FILE_PATH)
 
     # you can save the dataset for further analysis
-    logs.get_df().to_csv('src/datasets/categorical_df.csv')
+    records.get_df().to_csv('src/datasets/categorical_df.csv')
